9028598cfdb77a49f08c9c75f5ea242b6adf0f6c
BarbuLaura <117304669+BarbuLaura@users.noreply.github.com>
2022-11-05 17:33:10 +0200

    Add files via upload

--- 3buttons/script.js
+++ 3buttons/script.js
@@ -1,26 +1,6 @@
-let lucky = 0;
+var lucky = ["WINNER", "LOOSER"];
 
-function winningButton1(){
-    lucky += 3
-    if((lucky % 3) === 0){
-    document.getElementById("Winning Button-text").innerHTML = "WINNER"
-    }else{
-        document.getElementById("Winning Button-text").innerHTML = "LOOSER"
-    }
+function winningButton(){
+    document.getElementById("Winning Button-text").innerHTML = lucky[Math.floor(Math.random() * lucky.length)]
 }    
-function winningButton2(){
-    lucky += 2
-    if((lucky % 2) === 0){
-       document.getElementById("Winning Button-text").innerHTML = "WINNER"
-    } else{
-        document.getElementById("Winning Button-text").innerHTML = "LOOSER"
-    }
-}   
-function winningButton3(){
-    lucky +=5
-    if((lucky % 5) === 0){
-    document.getElementById("Winning Button-text").innerHTML = "WINNER"
-    } else {
-        document.getElementById("Winning Button-text").innerHTML = "LOOSER"
-    }
-}
+
